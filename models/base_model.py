e18e844f24b2d0c0fa8f2dbbaf0013281bfb04b6
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r1s14.42abudhabi.ae>
2023-07-12 21:49:52 +0400

    up

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,21 @@ from datetime import datetime
 
 class BaseModel:
     """for common attributes"""
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
     def __init__(self):
-        self.name = ''
         self.my_number = 0
+        self.name = ''
+        self.id = str(uuid.uuid4())
+        self.updated_at = datetime.now()
+        self.created_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        self.__dict__.update({'__class__': self.__class__.__name__})
+        # self.__dict__['__class__'] = self.__class__.__name__
+        return self.__dict__
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id, self.__dict__)
--- test_base_model.py
+++ test_base_model.py
@@ -4,10 +4,10 @@ from models.base_model import BaseModel
 my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
-print(my_model.my_number)
-print(my_model.name)
-# my_model.save()
-# print(my_model)
-# my_model_json = my_model.to_dict()
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
 # print(my_model_json)
 # print("JSON of my_model:")
