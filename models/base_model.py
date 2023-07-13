fe903fbcef389d1be40496908eb94cf4703f8ea0
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r5s6.42abudhabi.ae>
2023-07-13 19:14:12 +0400

    ex4-1

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -23,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
